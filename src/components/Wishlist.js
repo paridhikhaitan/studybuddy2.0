d7c6a8938235f03a72647fcbe1b4d470c1e73448
Paridhi Khaitan <paridhi4699@gmail.com>
2020-02-21 11:51:58 -0800

    logs

---
+++ src/components/Wishlist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Wishlist extends React.Component{
+    render(){
+        return(
+            <div>
+                WISHLIST
+            </div>
+        );
+    }
+}
+
+export default Wishlist;
