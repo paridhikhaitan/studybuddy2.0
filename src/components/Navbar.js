9b652f5d4ce7e4775b05fa5847ee17ce84c67c85
Paridhi Khaitan <paridhi4699@gmail.com>
2020-03-03 13:13:11 -0800

    added study buddy to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import '../scss/App.scss'
+import React from "react";
+import "../scss/App.scss";
+import { Row, Col } from "react-grid-system";
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <div className="Navbar">
-                <h4>
-                    <a href="/">
-                    home
-                    </a>
-                    
-                </h4>
-                <h4>
-                    <a href="/about">
-                        about
-                    </a>
-                </h4>
-                <button className="uploadBtn"><a href="/upload">upload</a></button>
-            </div>
-        );
-    }
+class Navbar extends React.Component {
+  render() {
+    return (
+      <Row>
+        <Col xs={8}>
+            <h1 id="nav"><span id="st">UCSD </span> study buddy</h1>
+        </Col>
+        <Col xs={8}>
+          <div className="Navbar">
+            <h4>
+              <a href="/">home</a>
+            </h4>
+            <h4>
+              <a href="/about">about</a>
+            </h4>
+            <button className="uploadBtn">
+              <a href="/upload">upload</a>
+            </button>
+          </div>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Navbar;
