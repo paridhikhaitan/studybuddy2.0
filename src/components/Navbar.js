7eef2011640a8666da2082e0bda667aee49ada77
paridhikhaitan <paridhi4699@gmail.com>
2020-03-11 15:30:05 -0700

    added login to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "../scss/App.scss";
-import { Row, Col } from "react-grid-system";
+import { Row, Col, Visible, Hidden } from "react-grid-system";
 
 class Navbar extends React.Component {
   render() {
     return (
       <Row>
-        <Col xs={8}>
-            <h1 id="nav"><span id="st">UCSD </span> study buddy</h1>
-        </Col>
+        <Hidden sm xs>
+          <Col xs={8}>
+            <h1 id="nav">
+              <span id="st">UCSD </span> study buddy
+            </h1>
+          </Col>
+        </Hidden>
         <Col xs={8}>
           <div className="Navbar">
             <h4>
@@ -16,6 +20,9 @@ class Navbar extends React.Component {
             </h4>
             <h4>
               <a href="/about">about</a>
+            </h4>
+            <h4>
+              <a href="/login">login</a>
             </h4>
             <button className="uploadBtn">
               <a href="/upload">upload</a>
